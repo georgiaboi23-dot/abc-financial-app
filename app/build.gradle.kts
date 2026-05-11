72e88b4ba40ed3e2e52ccebfe7ca43453ea6260e
georgiaboi23-dot <georgiaboi23@gmail.com>
2026-05-11 09:17:49 -0500

    production release
    Created production release 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.wovenminds.FinPhabet"
         minSdk = 26
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.2.5-beta"
+        versionCode = 12
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
